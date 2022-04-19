762320ccff5a1106fff9465b557d2fbb99b20e66
Your Name <you@example.com>
2022-04-19 17:22:37 -0300

    chalenge chapterIII

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,23 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
+    // Complete usando query builder ...
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games")
+      .where("games.title ILIKE :param", { param: `%${param}%` })
+      .getMany();        
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query ...
+    return this.repository.query("select count(*) from games"); 
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
+    // Complete usando query builder ...
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("game").where("game.id = :id", { id })
+      .relation(Game, "users").of(id).loadMany();
+      
   }
 }
